00b6da2349e369040d2da430e016b7119e04ae51
Parth <parth1493@gmail.com>
2019-08-24 15:24:27 -0400

    added JaCoCo code coverage Result and removed test code from ExampleUnitTest.java to ExampleInstrumentedTest.java

--- app/src/test/java/com/test/tddjunit5example/ExampleUnitTest.java
+++ app/src/test/java/com/test/tddjunit5example/ExampleUnitTest.java
@@ -1,10 +1,5 @@
 package com.test.tddjunit5example;
 
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -12,25 +7,5 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class ExampleUnitTest {
 
-    @Test
-    public void testAirport(){
 
-        Flight economyFlight = new Flight("1","Economy");
-        Flight businessFlight = new Flight("2","Business");
-
-        Passenger john = new Passenger("John", true);
-        Passenger mike = new Passenger("Mike", false);
-
-        assertEquals(true,economyFlight.addPassenger(john));
-        assertEquals(false,economyFlight.removePassenger(john));
-        assertEquals(true,businessFlight.addPassenger(john));
-        assertEquals(false,businessFlight.removePassenger(john));
-
-        assertEquals(true,economyFlight.addPassenger(mike));
-        assertEquals(true,economyFlight.removePassenger(mike));
-        assertEquals(false,businessFlight.addPassenger(mike));
-        assertEquals(false,businessFlight.removePassenger(mike));
-
-
-    }
 }
